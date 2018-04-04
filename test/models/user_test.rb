05b58d98deedf6bc4b6320e4b5dec76e011bcc3a
Yuta Nakamura <nakamuraytua0204@gmail.com>
2018-04-04 15:13:44 +0900

    Make a basic Usermodel (including secure passwords)

---
+++ test/models/user_test.rb
@@ -0,0 +1,20 @@
+require 'test_helper'
+
+class UserTest < ActiveSupport::TestCase
+
+  def setup
+	  @user = User.new(name:"Eample User", email:"user@example.com", password: "foobar", password_confirmation: "foobar")
+	end
+
+ test "email validation shoule accept valid addreses" do 
+	 valid_addresses = %w[user@example,com U*S~ER@foo.COM A_US-ER_foo.bar.org first.lastfoo.jp alice+bob@baz,cn]
+
+
+	 valid_addresses.each do |valid_address|
+		 @user.email = valid_address
+		 assert_not @user.valid?, "#{valid_address.inspect} should be valid"
+	 end
+ end
+
+end
+
