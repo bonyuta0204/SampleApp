d15001561d120ef54b405149c398aafed780d4f0
Yuta Nakamura <nakamuraytua0204@gmail.com>
2018-04-13 10:53:40 +0900

    Add login

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,20 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      # admit login. redirect to user page
+      log_in user
+      redirect_to user
+    else
+      # error
+      flash.now[:danger] = "Invalid/password combination"
+    render 'new'
+    end
+  end
+
+  def destroy
+  end
+end
