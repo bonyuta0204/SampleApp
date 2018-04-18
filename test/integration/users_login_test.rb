84f3b7f1c6ca24f2e73e0ab5ffa6217030fb03c0
Yuta Nakamura <nakamuraytua0204@gmail.com>
2018-04-18 23:23:23 +0900

    Finish Permanent Login

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -6,6 +6,19 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     @user = users(:michael)
   end
 
+  test "login with remembering" do
+    log_in_as(@user,  remember_me:'1')
+    assert_equal cookies['remember_token'],  assigns(:user).remember_token
+  end
+
+  test "login without remembering" do
+    log_in_as(@user,  remember_me: '1')
+    delete logout_path
+
+    log_in_as(@user,  remember_me:'0')
+    assert_empty cookies['remember_token']
+  end
+
   test "loginwith invalid information" do
     get login_path
     assert_template "sessions/new"
