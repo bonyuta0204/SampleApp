4b5f67846fc97455457d13f0a349e2a269563ab6
Yuta Nakamura <nakamuraytua0204@gmail.com>
2018-06-25 05:27:49 +0900

    Add relation model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,24 @@
 class User < ApplicationRecord
   has_many :microposts, dependent: :destroy
+  has_many :active_relationships, class_name: "Relationship", 
+    foreign_key: "follower_id", 
+    dependent: :destroy
+  has_many :following, through: :active_relationships,  source: :followed
+
   default_scope -> { order(created_at: :desc) }
+
   attr_accessor :remember_token, :activation_token, :reset_token
+
   before_save :downcase_email
   before_create :create_activation_digest
+
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
 
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
+
 
   def self.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt:: Engine.cost
