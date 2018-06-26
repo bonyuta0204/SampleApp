1d20c490efd9fcc05850a52d537c0193611237dd
Yuta Nakamura <nakamuraytua0204@gmail.com>
2018-06-26 18:00:45 +0900

    Add unfavorite feature

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,5 +1,6 @@
 class FavoritesController < ApplicationController
-  before_action :logged_in_user, only: %i[delete, create]
+  before_action :logged_in_user, only: %i[destroy create]
+  before_action :correct_user, only: %i[destroy]
 
 def create
   @micropost = Micropost.find(params[:micropost_id])  
@@ -7,8 +8,18 @@ def create
   redirect_to root_path
 end
 
-def delete
+def destroy
+  @favorite = Favorite.find(params[:id])
+  @favorite.destroy
+  redirect_to root_path
 end
 
+private
+
+def correct_user
+  @favorite = current_user.favorites.find_by(id: params[:id])
+  redirect_to root_url if @favorite.nil?
+
+end
 
 end
--- test/controllers/favorites_controller_test.rb
+++ test/controllers/favorites_controller_test.rb
@@ -5,15 +5,36 @@ class FavoritesControllerTest < ActionDispatch::IntegrationTest
   test 'should redirect create when not logged in' do
     archer = users(:archer)
     orange = microposts(:orange)
-    
     assert_no_difference 'Favorite.count' do
       post favorites_path, params: {micropost_id: orange.id}
     end
-
     log_in_as(archer)
     assert_difference 'Favorite.count', 1 do
       post favorites_path, params: {micropost_id: orange.id}
     end
   end
 
+  test 'should redirect delete when not logged in' do
+    michael = users(:michael)
+    orange = microposts(:orange)
+    @favorite = favorites(:one)
+    assert_no_difference 'Favorite.count' do
+      delete  favorite_path(@favorite)
+    end
+    log_in_as(michael)
+    assert_difference 'Favorite.count', -1 do
+      delete  favorite_path(@favorite)
+    end
+  end
+
+  test 'should redirect delete when different_user' do
+    michael = users(:michael)
+    archer = users(:archer)
+    orange = microposts(:orange)
+    @favorite = favorites(:one)
+    log_in_as(archer)
+    assert_no_difference 'Favorite.count' do
+      delete  favorite_path(@favorite)
+    end
+  end
 end
