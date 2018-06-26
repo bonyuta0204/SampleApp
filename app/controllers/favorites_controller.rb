6071c8baa62e23b71ba88728fb6b2eef9b68fec8
Yuta Nakamura <nakamuraytua0204@gmail.com>
2018-06-26 20:30:19 +0900

    Add ajax to favorite button

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -2,24 +2,30 @@ class FavoritesController < ApplicationController
   before_action :logged_in_user, only: %i[destroy create]
   before_action :correct_user, only: %i[destroy]
 
-def create
-  @micropost = Micropost.find(params[:micropost_id])  
-  current_user.favorite(@micropost)
-  redirect_to root_path
-end
+  def create
+    @micropost = Micropost.find(params[:micropost_id])  
+    current_user.favorite(@micropost)
+    respond_to do |format|
+      format.html { redirect_to @user}
+      format.js
+    end
+  end
 
-def destroy
-  @favorite = Favorite.find(params[:id])
-  @favorite.destroy
-  redirect_to root_path
-end
+  def destroy
+    @favorite = Favorite.find(params[:id])
+    @favorite.destroy
+    respond_to do |format|
+      format.html { redirect_to @user}
+      format.js
+    end
+  end
 
-private
+  private
 
-def correct_user
-  @favorite = current_user.favorites.find_by(id: params[:id])
-  redirect_to root_url if @favorite.nil?
+  def correct_user
+    @favorite = current_user.favorites.find_by(id: params[:id])
+    redirect_to root_url if @favorite.nil?
 
-end
+  end
 
 end
